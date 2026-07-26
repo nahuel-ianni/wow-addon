3c67f0fc567110e92457fdb955c7e0502b933f3b
Nahuel Ianni <navi@Nahuels-MacBook-Pro.local>
2026-07-26 23:10:45 +0200

    Code clean up

--- src/modules/fishing.lua
+++ src/modules/fishing.lua
@@ -5,67 +5,46 @@ local Module = ns.Addon:NewModule("FISHING", "AceEvent-3.0")
 --  Local Variables
 -- ─────────────────────────────────────────────────────────────────────────────────
 
-local PLAYER = "player"
+local UNIT = "player"
+local CVAR = "SoftTargetInteract"
+local TARGET = "INTERACTTARGET"
 local K_FISHING = "KEY_FISHING"
-local FISHING_SPELL = _G.C_Spell.GetSpellName(131474)
-local INTERACT_TARGET = "INTERACTTARGET"
-
+local SPELL_NAME = _G.C_Spell.GetSpellName(131474)
+local BINDINGS = {
+    CAST = "CAST",
+    INTERACT = "INTERACT"
+}
 local FISHING_SPELLS = {
-    [7620] = true,
     [131474] = true,
     [131476] = true,
-    [131490] = true,
-}
-local SOFT_TARGET = {
-    CVAR = "SoftTargetInteract",
-    VALUE = "3"
+    [131490] = true, 
+    [7620]   = true,
 }
 
 local After = _G.C_Timer.After
-local GetCVar = _G.GetCVar
-local SetCVar = _G.SetCVar
 local InCombatLockdown = _G.InCombatLockdown
 local SetOverrideBinding = _G.SetOverrideBinding
 local ClearOverrideBindings = _G.ClearOverrideBindings
 local SetOverrideBindingSpell = _G.SetOverrideBindingSpell
 
 local frame = _G.CreateFrame("Frame")
-local cachedCVar
+local cachedInteract
 
 -- ─────────────────────────────────────────────────────────────────────────────────
 --  Local Functions
 -- ─────────────────────────────────────────────────────────────────────────────────
 
-local function ApplyBinding(interact)
+local function SetBinding(mode)
     if InCombatLockdown() then return end
-
-    if interact then
-        SetOverrideBinding(frame, false, Module.Options:Get(K_FISHING), INTERACT_TARGET)
+    
+    if mode == BINDINGS.INTERACT then
+        cachedInteract = _G.GetCVar(CVAR)
+        if cachedInteract ~= "3" then _G.SetCVar(CVAR, "3") end
+        SetOverrideBinding(frame, false, Module.Options:Get(K_FISHING), TARGET)
     else
-        SetOverrideBindingSpell(frame, false, Module.Options:Get(K_FISHING), FISHING_SPELL)
+        if cachedInteract then _G.SetCVar(CVAR, cachedInteract) end
+        SetOverrideBindingSpell(frame, false, Module.Options:Get(K_FISHING), SPELL_NAME)
     end
-end
-
-local function SetInteractCVar(enable)
-    if enable then
-        cachedCVar = cachedCVar or GetCVar(SOFT_TARGET.CVAR)
-        return SetCVar(SOFT_TARGET.CVAR, SOFT_TARGET.VALUE)
-    end
-
-    if not cachedCVar then return end
-
-    SetCVar(SOFT_TARGET.CVAR, cachedCVar)
-    cachedCVar = nil
-end
-
-local function StartFishing()
-    ApplyBinding(true)
-    SetInteractCVar(true)
-end
-
-local function StopFishing()
-    ApplyBinding(false)
-    SetInteractCVar(false)
 end
 
 -- ─────────────────────────────────────────────────────────────────────────────────
@@ -73,16 +52,15 @@ end
 -- ─────────────────────────────────────────────────────────────────────────────────
 
 function Module:OnEnable()
-    self:RegisterEvent("PLAYER_REGEN_ENABLED")
     self:RegisterEvent("UNIT_SPELLCAST_CHANNEL_START")
     self:RegisterEvent("UNIT_SPELLCAST_CHANNEL_STOP")
+    self:RegisterEvent("PLAYER_REGEN_ENABLED")
 
-    ApplyBinding(false)
+    SetBinding(BINDINGS.CAST)
 end
 
 function Module:OnDisable()
     ClearOverrideBindings(frame)
-    SetInteractCVar(false)
     self:UnregisterAllEvents()
 end
 
@@ -90,14 +68,14 @@ function Module:InjectOptions()
     self.Options:AddKeybind(K_FISHING)
 end
 
-function Module:PLAYER_REGEN_ENABLED()
-    ApplyBinding(cachedCVar ~= nil)
-end
-
 function Module:UNIT_SPELLCAST_CHANNEL_START(_, unit, _, spellID)
-    if unit == PLAYER and FISHING_SPELLS[spellID] then StartFishing() end
+    if unit == UNIT and FISHING_SPELLS[spellID] then SetBinding(BINDINGS.INTERACT) end
 end
 
 function Module:UNIT_SPELLCAST_CHANNEL_STOP(_, unit, _, spellID)
-    if unit == PLAYER and FISHING_SPELLS[spellID] then After(0.5, StopFishing) end
+    if unit == UNIT and FISHING_SPELLS[spellID] then After(0.5, SetBinding) end
+end
+
+function Module:PLAYER_REGEN_ENABLED()
+    SetBinding(BINDINGS.CAST)
 end
